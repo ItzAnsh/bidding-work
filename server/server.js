55940a612359be7edb9f4ea9cde3964d58d5167a
Ansh Bhasin <74207421+ItzAnsh@users.noreply.github.com>
2024-05-02 16:38:53 +0000

    done

--- server/server.js
+++ server/server.js
@@ -1,60 +1,67 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io').Server;
-const mongoose = require('mongoose');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io").Server;
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = new socketIo(server, {
-    cors:{
-        origin:"*"
-    }
+	cors: {
+		origin: "*",
+	},
 });
 
-mongoose.connect('mongodb+srv://Ansh:iZbKKbr8Sq6mgbsh@ceed-databse-events.7zoqyvb.mongodb.net/socket/').then(() => 
-  console.log('Connected to MongoDB')
-).catch(err => console.error('Could not connect to MongoDB', err));
+// cors
+app.use(cors());
+
+mongoose
+	.connect(
+		"mongodb+srv://Ansh:iZbKKbr8Sq6mgbsh@ceed-databse-events.7zoqyvb.mongodb.net/socket"
+	)
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.error("Could not connect to MongoDB", err));
 
 const messageSchema = new mongoose.Schema({
-  room: String,
-  user: String,
-  text: String,
-  createdAt: { type: Date, default: Date.now }
+	room: String,
+	user: String,
+	text: String,
+	createdAt: { type: Date, default: Date.now },
 });
 
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 const rooms = new Set();
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
+io.on("connection", (socket) => {
+	console.log("New client connected");
 
-  io.emit('updateRooms', Array.from(rooms));
+	io.emit("updateRooms", Array.from(rooms));
 
-  socket.on('createRoom', (roomName) => {
-    rooms.add(roomName);
-    io.emit('updateRooms', Array.from(rooms));
-  });
+	socket.on("createRoom", (roomName) => {
+		rooms.add(roomName);
+		io.emit("updateRooms", Array.from(rooms));
+	});
 
-  socket.on('joinRoom', (roomName) => {
-    socket.join(roomName);
-    socket.room = roomName;
-  });
+	socket.on("joinRoom", (roomName) => {
+		socket.join(roomName);
+		socket.room = roomName;
+	});
 
-  socket.on('sendMessage', async (message) => {
-    const { user, text } = message;
-    console.log(`Received message from ${user}: ${text}`);
-    const newMessage = new Message({ room: socket.room, user, text });
-    await newMessage.save();
-    io.to(socket.room).emit('message', newMessage);
-  });
+	socket.on("sendMessage", async (message) => {
+		const { user, text } = message;
+		console.log(`Received message from ${user}: ${text}`);
+		const newMessage = new Message({ room: socket.room, user, text });
+		await newMessage.save();
+		io.to(socket.room).emit("message", newMessage);
+	});
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
+	socket.on("disconnect", () => {
+		console.log("Client disconnected");
+	});
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
